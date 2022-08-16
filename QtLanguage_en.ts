7f2a2d0122e87b3be5fc8ab2fdf22b053238e2cd
MileX0k <81148586+MileX0k@users.noreply.github.com>
2022-08-16 11:52:02 +0500

    Lesson 6
    Добавил урок 6:
    1)Добавил меню
    2)Добавил возможность печати
    3)Подправил недочеты
    
    Из минусов:
    Не сделал перевода меню на английский, не понимаю что делаю не так(

---
+++ QtLanguage_en.ts
@@ -0,0 +1,140 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="20"/>
+        <location filename="mainwindow.cpp" line="177"/>
+        <source>Главное окно</source>
+        <translation>Main window</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="41"/>
+        <location filename="mainwindow.cpp" line="180"/>
+        <source>Открыть</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="62"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Нажмите для подсказки&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Click for a hint&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="68"/>
+        <location filename="mainwindow.cpp" line="178"/>
+        <source>Справка</source>
+        <translation>Description</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="92"/>
+        <location filename="mainwindow.cpp" line="179"/>
+        <source>Сохранить</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="129"/>
+        <location filename="mainwindow.cpp" line="181"/>
+        <source>Только для чтения</source>
+        <translation>Read-only</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="149"/>
+        <location filename="mainwindow.cpp" line="183"/>
+        <source>Русский</source>
+        <translation>Russian</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="154"/>
+        <location filename="mainwindow.cpp" line="184"/>
+        <source>Английский</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="171"/>
+        <location filename="mainwindow.cpp" line="185"/>
+        <source>Светлая тема</source>
+        <translation>Light theme</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="184"/>
+        <location filename="mainwindow.cpp" line="186"/>
+        <source>Темная тема</source>
+        <translation>Dark theme</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="12"/>
+        <source>Файл</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="14"/>
+        <source>Новый файл</source>
+        <translation>New file</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="18"/>
+        <source>Открыть...</source>
+        <translation>Open...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="22"/>
+        <source>Сохранить...</source>
+        <translation>Save...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="26"/>
+        <source>Печать...</source>
+        <translation>Print...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="30"/>
+        <source>Выход</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="42"/>
+        <source>Окно сохранения</source>
+        <translation>Save window</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="69"/>
+        <source>Окно открытия</source>
+        <translation>Open window</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="122"/>
+        <source>Вы уверены?</source>
+        <translation>Are you sure?</translation>
+    </message>
+    <message>
+        <source>Создать новый документ?</source>
+        <translation type="vanished">Create a new document?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="123"/>
+        <source>Все несохраненные данные будут утеряны!</source>
+        <translation>All unsaved data will be lost!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="124"/>
+        <source>Да</source>
+        <translation>Yes</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="125"/>
+        <source>Нет</source>
+        <translation>No</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="148"/>
+        <source>Печать</source>
+        <translation>Print</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="182"/>
+        <source>Нажмите для подсказки</source>
+        <translation>Click for a hint</translation>
+    </message>
+</context>
+</TS>
